cd1457d6782dde4606a13276f35cbf9a7c5ab16e
juanmism <juanmism@gmail.com>
2022-01-03 11:53:11 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function randomExcuse() {
+  let whoNumber = Math.floor(Math.random() * 4);
+  let actionNumber = Math.floor(Math.random() * 4);
+  let whatNumber = Math.floor(Math.random() * 3);
+  let whenNumber = Math.floor(Math.random() * 5);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.getElementById("excuse").innerHTML =
+    who[whoNumber] +
+    " " +
+    action[actionNumber] +
+    " " +
+    what[whatNumber] +
+    " " +
+    when[whenNumber];
+}
+window.onload = randomExcuse();
